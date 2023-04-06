69db4e1c8eb4418437e98c048c8563cf88bab084
Emersson25 <ortizjacobo25@gmail.com>
2023-04-06 12:12:32 -0500

    Partimos el Header y el footer del index hacia la carpeta templates

--- index.php
+++ index.php
@@ -1,53 +1,9 @@
-<!doctype html>
-<html lang="es">
+<?php include("templates/header.php"); ?>
 
-<head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-</head>
-
-<body>
-  <header>
-
-    <!-- place navbar here -->
-  </header>
-
-  <nav class="navbar navbar-expand navbar-light bg-light">
-      <ul class="nav navbar-nav">
-          <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">Sistema <span class="visually-hidden">(current)</span></a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Contacto</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Categoria</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Notas</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Compañia</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Cerrar Seccion</a>
-          </li>
-          
-      </ul>
-  </nav>
-
-  <main class="container">
 
   </br>
 
-  
+
     <div class="p-5 mb-4 bg-light rounded-3">
         <div class="container-fluid py-5">
           <h1 class="display-5 fw-bold">Custom jumbotron</h1>
@@ -57,22 +13,10 @@
       </div>
 
 
+      <?php include("templates/footer.php"); ?>
 
 
 
-  </main>
 
-  <footer>
-    <!-- place footer here -->
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>
+  
